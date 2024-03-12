92b25799268ea3e951834ddeef021e28785373e9
samuel-welborn <pokingvictim@protonmail.com>
2024-03-11 19:50:08 -0700

    git

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,5 +1,6 @@
 let
-  cmde = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOq9Gew1rgfdIyuriJ/Ne0B8FE1s8O/U2ajErVQLUDu9 cmde@io";
+  cmde = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO+ZdcDI7nryzyzhAiCnRlnU7+0Nzlr4n6rAgW4hFbxT pokingvictim@protonmail.com
+";
 
   io = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIFMR4XHc7mhSs0Diy2gWtXurueQiQ1gKjyzW2fuqtqv root@io";
 in {
